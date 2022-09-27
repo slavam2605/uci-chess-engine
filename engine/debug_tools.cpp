0492f2ee5f0b6773a0422f56bcfab3026d244938
slavam2605 <slavam2605@bk.ru>
2022-09-28 00:49:47 +0300

    Fix compilation

--- engine/debug_tools.cpp
+++ engine/debug_tools.cpp
@@ -12,12 +12,13 @@ void debug_tools::print_values_for_moves(const string& fen) {
     move_list moves;
     chess_move_generator::generate_all_moves(moves, state, state.side);
     transposition_table table;
+    killer_table ktable;
     dynamic_evaluator evaluator;
     cout << "initial value: " << static_evaluator::evaluate(state) << " cp" << endl;
     for (const auto& move : moves) {
         game_state new_state(state);
         new_state.apply_move(move);
-        auto value = evaluator.pvs(new_state, table, 6, 1, -1000000000, 1000000000,
+        auto value = evaluator.pvs(new_state, table, ktable, 6, 1, -1000000000, 1000000000,
                                    new_state.side == chess::White ? 1 : -1);
         cout << move << " => " << /*static_evaluator::evaluate(new_state)*/value << " cp" << endl;
     }
