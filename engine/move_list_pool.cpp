16a5ad12de5e802e86c6d3cf40d1bea4b3aa0644
slavam2605 <slavam2605@bk.ru>
2022-09-28 00:48:42 +0300

    Increase size of move list pool and add assertions on its size

--- engine/move_list_pool.cpp
+++ engine/move_list_pool.cpp
@@ -1,6 +1,7 @@
 #include "move_list_pool.h"
 
 move_list& move_list_pool::init_list(int index) {
+    Assert(index < pool.size())
     auto& list = pool[index];
     list.clear();
     return list;
--- engine/move_list_pool.h
+++ engine/move_list_pool.h
@@ -5,7 +5,7 @@
 #include "move_list.h"
 
 class move_list_pool {
-    std::array<move_list, 250> pool{};
+    std::array<move_list, 1000> pool{};
 public:
     move_list& init_list(int index);
 };
