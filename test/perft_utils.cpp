dba7b3ecb7aebf2b70e5c666b6ed798f9b063899
slavam2605 <slavam2605@bk.ru>
2022-09-28 00:47:52 +0300

    Add move list pool to the perft util

--- test/perft_utils.cpp
+++ test/perft_utils.cpp
@@ -5,28 +5,29 @@ using namespace std;
 std::vector<pair<chess_move, size_t>> perft_utils::perft(const string& fen, int depth) {
     vector<pair<chess_move, size_t>> result;
     game_state state(fen);
-    move_list moves;
+    move_list_pool pool;
+    move_list& moves = pool.init_list(depth);
     chess_move_generator::generate_all_moves(moves, state, state.side);
     for (const auto& move: moves) {
         game_state new_state(state);
         new_state.apply_move(move);
-        auto nodes = perft_inner(new_state, depth - 1);
+        auto nodes = perft_inner(new_state, pool, depth - 1);
         result.emplace_back(move, nodes);
     }
     return result;
 }
 
-size_t perft_utils::perft_inner(const game_state& state, int depth) { // NOLINT(misc-no-recursion)
+size_t perft_utils::perft_inner(const game_state& state, move_list_pool& pool, int depth) { // NOLINT(misc-no-recursion)
     if (depth == 0) return 1;
     size_t result = 0;
-    move_list moves;
+    move_list& moves = pool.init_list(depth);
     chess_move_generator::generate_all_moves(moves, state, state.side);
     if (depth == 1) return moves.size();
     for (const auto& move: moves) {
         game_state new_state(state);
         new_state.apply_move(move);
         if (depth > 2) {
-            result += perft_inner(new_state, depth - 1);
+            result += perft_inner(new_state, pool, depth - 1);
         } else {
             move_list new_moves;
             chess_move_generator::generate_all_moves(new_moves, new_state, new_state.side);
--- test/perft_utils.h
+++ test/perft_utils.h
@@ -3,10 +3,11 @@
 
 #include "../engine/chess_move.h"
 #include "../engine/chess_move_generator.h"
+#include "../engine/move_list_pool.h"
 #include <vector>
 
 class perft_utils {
-    static size_t perft_inner(const game_state& state, int depth);
+    static size_t perft_inner(const game_state& state, move_list_pool& pool, int depth);
 public:
     static std::vector<std::pair<chess_move, size_t>> perft(const std::string& fen, int depth);
 };
